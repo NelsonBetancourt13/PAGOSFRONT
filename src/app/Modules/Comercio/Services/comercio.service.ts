85f3b382bab8d607db140b7206bfb0826e6233aa
Nelson <nelbelo2018@gmail.com>
2023-11-14 23:32:03 -0500

    VISUALIZACION DE DATOS COMPLETA

--- src/app/Modules/Comercio/Services/comercio.service.ts
+++ src/app/Modules/Comercio/Services/comercio.service.ts
@@ -1,34 +1,21 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 
-
-
 const base_url = "http://localhost:8080/api/Prueba/Consulta";
-
-
 
 
 @Injectable({
   providedIn: 'root'
 })
+export class ComercioService {
 
-
-export class PagadorService {
 
   constructor(private http: HttpClient) { }
 
-//OBTIENE TODOS LOS USUARIOS ROL_PAGADOR
-  getUsuarioPagador(){
-
-    const endpoint = `${base_url}/Rol_Pagador`;
-    return this.http.get(endpoint);
-  }
 
   getUsuarioComercio(){
 
-    const endpoint = `${base_url}/comercio`;
+    const endpoint = `${base_url}/Rol_Comercio`;
     return this.http.get(endpoint);
-  }
 
-
-}
+  }}
--- src/app/Modules/Pagador/Component/pagador/pagador.component.ts
+++ src/app/Modules/Pagador/Component/pagador/pagador.component.ts
@@ -2,6 +2,7 @@ import {Component, inject, OnInit} from '@angular/core';
 import {PagadorService} from "../../../Share/Services/pagador.service";
 import {MatTableDataSource} from "@angular/material/table";
 
+
 @Component({
   selector: 'app-pagador',
   templateUrl: './pagador.component.html',
@@ -18,11 +19,11 @@ constructor(private pagadorService: PagadorService) {
 
   ngOnInit(): void {
     this.getUsuarioPagador();
+    this.getUsuarioComercio();
   }
 
 
-displayedColumns: string[] = ["id","usuario_nombre", "usuario_identificacion",
-  "usuario_gmail","rol_p", "trans_codigo", "trans_medio_pago",
+displayedColumns: string[] = ["id","trans_codigo", "trans_medio_pago",
   "trans_estado", "trans_total", "trans_fecha", "trans_concepto", "actions" ];
 dataSource = new MatTableDataSource<PagadorElement>();
 
@@ -32,6 +33,8 @@ dataSource = new MatTableDataSource<PagadorElement>();
       .subscribe(data => {
         console.log("respuesta usuario ", data);
         this.processPagadorResponse(data);
+
+
     }, error =>{
 
         console.log(" Error ", error);
@@ -43,36 +46,77 @@ dataSource = new MatTableDataSource<PagadorElement>();
   processPagadorResponse(resp:any){
 
     const dataPagador: PagadorElement[] = [];
-
     let listPagador = resp;
 
-    listPagador.forEach((element:PagadorElement) =>{
-      element.rol_p = element.rol_p.name;
-      element.trans_total = element.rol_p.id;
+    listPagador[0].pagos.forEach((element:PagadorElement) =>{
       dataPagador.push(element);
-    this.dataSource = new MatTableDataSource<PagadorElement>(dataPagador);
-
-
-
+      console.log(element.comercio);
 
     });
+    this.dataSource = new MatTableDataSource<PagadorElement>(dataPagador);
+
+  }
+
+  //********* CONSUMIMOS EL SERVICIO DE GET COMERCIO *************///
+
+  displayedColumns1: string[] = ["id","comercio_codigo","comercio_nombre",
+    "comercio_direccion", "comercio_nit" , "actions" ];
+  dataSource1 = new MatTableDataSource<ComercioElement>();
+
+
+
+  getUsuarioComercio(){
+
+    this.pagadorService.getUsuarioComercio()
+      .subscribe(data => {
+        console.log("respuesta usuario ", data);
+        this.processComercioResponse(data);
+
+
+      }, error =>{
+
+        console.log(" Error ", error);
+      })
+
+  }
+
+  processComercioResponse(resp:any){
+
+    const dataComercio: ComercioElement[] = [];
+    let listComercio = resp;
+
+    listComercio.forEach((element:ComercioElement) =>{
+      dataComercio.push(element);
+
+    });
+    this.dataSource1 = new MatTableDataSource<ComercioElement>(dataComercio);
+
   }
 
 
 }
 
 export interface PagadorElement{
-  id:number;
-  usuario_nombre: string;
-  usuario_identificacion: string;
-  usuario_gmail: string;
-  rol_p: any;
-  trans_codigo: any;
-  trans_medio_pago: any;
-  trans_estado: any;
-  trans_total: any;
-  trans_fecha: any;
-  trans_concepto: any;
 
+  id: number;
+  trans_codigo: number;
+  trans_concepto: string;
+  trans_estado: number;
+  trans_fecha: string;
+  trans_medio_pago: number;
+  trans_total: number;
+  comercio: any;
+
+
+}
+
+
+export interface ComercioElement {
+
+  id: number;
+  comercio_codigo: number;
+  comercio_nombre: string;
+  comercio_nit: string;
+  comercio_direccion: string;
 
 }
